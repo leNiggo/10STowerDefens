0d663f63799514949eb1ddb44399e5ae49374ceb
Nico Eberl <n.eberl@tiffinger-thiel.de>
2022-10-01 15:57:02 +0200

    🎉 Spawn first enemy

--- Scenes/Enemy/Enemy.gd
+++ Scenes/Enemy/Enemy.gd
@@ -1,19 +1,16 @@
-extends Node2D
+extends PathFollow2D
 
 
 @export_range(0, 100) var live: int = 50;
 @export_range(0, 1000) var damage: int = 50;
 @export_range(0, 100) var speed: int = 20;
 
-var tile_map: TileMap;
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	tile_map = get_parent()
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
-	
+func _physics_process(_delta):
+	h_offset += _delta * speed
 	pass
--- Scenes/GameScene/GameScene.gd
+++ Scenes/GameScene/GameScene.gd
@@ -1,11 +1,21 @@
 extends Node2D
 
-
+var map_node: Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	map_node = get_node("/root/LevelOne")
+	spawn_enemy()
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	pass
+
+
+func spawn_enemy():
+	var new_enemy = preload("res://Scenes/Enemy/Enemy.tscn").instantiate()
+	map_node.get_node('EnemyPath').add_child(new_enemy, true)
+	pass
+
+
