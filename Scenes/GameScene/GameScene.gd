07426df8e682d0da302c04125b461f3ac87def9b
Nico Eberl <n.eberl@tiffinger-thiel.de>
2022-10-01 15:23:09 +0200

    ✨ Add enemy scene and game scene and path

--- Scenes/GameScene/GameScene.gd
+++ Scenes/GameScene/GameScene.gd
@@ -1,19 +1,11 @@
 extends Node2D
 
 
-@export_range(0, 100) var live: int = 50;
-@export_range(0, 1000) var damage: int = 50;
-@export_range(0, 100) var speed: int = 20;
-
-var tile_map: TileMap;
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	tile_map = get_parent()
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
-	
+func _process(delta):
 	pass
