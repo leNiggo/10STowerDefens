7f4d001cf573a21ea1e082439b0a3f43ea52aadb
Nico Eberl <n.eberl@tiffinger-thiel.de>
2022-10-01 22:14:52 +0200

    ✨ Add connector for spawn buttons

--- Scenes/GameScene/GameScene.gd
+++ Scenes/GameScene/GameScene.gd
@@ -1,14 +1,33 @@
 extends Node2D
 
 var map_node: Node2D
+
+var build_mode: bool = false;
+var build_valid: bool = false;
+var build_location: Vector2;
+var build_type: String;
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	map_node = get_node("/root/LevelOne") # Replace this with an variable so it can be used to multiple maps
 	spawn_enemy()
-	pass # Replace with function body.
+	for i in get_tree().get_nodes_in_group('build_turrets'):
+		var callable = Callable(self, 'initiate_build_mode')
+		callable.call(i.name)
+		i.connect("pressed", callable)
+		
+	pass
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+func initiate_build_mode(tower_type: String): 
+	build_type = tower_type
+	build_mode = true;
+	get_parent().get_node('BuildTurretUi').set_tower_preview(build_type, get_global_mouse_position())
+	get_node('UI')
+	pass
+
+
 func _process(_delta):
 	pass
 
@@ -17,5 +36,4 @@ func spawn_enemy():
 	var new_enemy = preload("res://Scenes/Enemy/Enemy.tscn").instantiate()
 	map_node.get_node('EnemyPath').add_child(new_enemy, true)
 	pass
-
 
---
+++ UI/BuildTurretUI/BuildTurretUi.gd
@@ -0,0 +1,13 @@
+extends CanvasLayer
+
+func set_tower_preview(tower_type: String, mouse_position: Vector2): 
+	var drag_tower = load("res://Scenes/Towers/" + tower_type + ".tscn").instantiate();
+	drag_tower.set_name("DragTower")
+	drag_tower.modulate = Color("ad54ff3c")
+	
+	var controll = Control.new()
+	controll.add_child(drag_tower, true)
+	controll.position = mouse_position
+	controll.set_name("TowerPreview")
+	add_child(controll, true)
+	pass
